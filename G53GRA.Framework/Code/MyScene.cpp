7f57d7f7fd010cd0bc065257c9ea82215308b4ea
Himmy Kong <himmykch2014@gmail.com>
2019-05-06 10:42:01 +0100

    d

--- G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/Code/MyScene.cpp
@@ -22,7 +22,7 @@ void MyScene::Initialise()
     //-----------------------------------Texture-----------------------------------
     GLuint ft = Scene::GetTexture("stars.bmp"); //give options to change it to other bmp
     GLuint bk = Scene::GetTexture("stars.bmp"); //but decided on using only star.bmp for skybox
-    GLuint lf = Scene::GetTexture("grass2.bmp");
+    GLuint lf = Scene::GetTexture("stars.bmp");
     GLuint rt = Scene::GetTexture("stars.bmp");
     GLuint up = Scene::GetTexture("stars.bmp");
     GLuint dn = Scene::GetTexture("grass3.bmp");
